74da3f43ccd32ab853c33e1af8bde65044f4da5a
Binand Sethumadhavan <binand@radongames.in>
2024-09-08 12:05:45 +0530

    Added Application class to enable Hilt (needs @HiltAndroidApp on Application class)

---
+++ app/src/main/java/org/subinium/smstoemail/SmsForwarderApplication.java
@@ -0,0 +1,9 @@
+package org.subinium.smstoemail;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class SmsForwarderApplication extends Application {
+}
