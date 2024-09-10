925bdef153f693337809a9562f14eca034a596a9
Binand Sethumadhavan <binand@radongames.in>
2024-09-10 21:10:24 +0530

    Switched to viewbinding.

--- ReceiverApp/src/main/java/in/radongames/smsreceiver/ReceiverActivity.java
+++ ReceiverApp/src/main/java/in/radongames/smsreceiver/ReceiverActivity.java
@@ -7,30 +7,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.messaging.FirebaseMessaging;
-import com.radongames.android.platform.Toaster;
-
-import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
+import in.radongames.smsreceiver.databinding.ActivityReceiverBinding;
 import lombok.CustomLog;
 
 @AndroidEntryPoint
 @CustomLog
 public class ReceiverActivity extends AppCompatActivity {
 
-    @Inject
-    Toaster mToaster;
+    private ActivityReceiverBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_receiver);
+        mBinding = ActivityReceiverBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
 
         processLayout();
     }
@@ -47,7 +45,7 @@ public class ReceiverActivity extends AppCompatActivity {
 
         if (item.getItemId() == R.id.action_copy_token) {
 
-            copyMyFcmTokenToClipboard();
+            copyFcmTokenToClipboard();
         }
 
         return true;
@@ -55,26 +53,22 @@ public class ReceiverActivity extends AppCompatActivity {
 
     private void processLayout() {
 
-        final EditText etServer = findViewById(R.id.et_server);
-        final EditText etUsername = findViewById(R.id.et_username);
-        final EditText etPassword = findViewById(R.id.et_password);
-
         PrefManager pm = PrefManager.getInstance(this);
-        etServer.setText(pm.getServer());
-        etUsername.setText(pm.getUsername());
-        etPassword.setText(pm.getPassword());
+        mBinding.etServer.setText(pm.getServer());
+        mBinding.etUsername.setText(pm.getUsername());
+        mBinding.etPassword.setText(pm.getPassword());
 
         Button b = findViewById(R.id.b_save);
         b.setOnClickListener(v -> {
 
             PrefManager pm1 = PrefManager.getInstance(getApplicationContext());
-            pm1.setServer(etServer.getText().toString());
-            pm1.setUsername(etUsername.getText().toString());
-            pm1.setPassword(etPassword.getText().toString());
+            pm1.setServer(mBinding.etServer.getText().toString());
+            pm1.setUsername(mBinding.etUsername.getText().toString());
+            pm1.setPassword(mBinding.etPassword.getText().toString());
         });
     }
 
-    protected void copyMyFcmTokenToClipboard() {
+    protected void copyFcmTokenToClipboard() {
 
         FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
 
