a269490708fdb5364d102af56ee5759c5b73ad49
Binand Sethumadhavan <binand@radongames.in>
2024-09-10 09:59:53 +0530

    Moved SmsBroadcastReceiver back to ForwarderApp - it was shifted to ReceiverApp by mistake.

--- ForwarderApp/src/main/java/in/radongames/smsforwarder/SmsBroadcastReceiver.java
+++ ForwarderApp/src/main/java/in/radongames/smsforwarder/SmsBroadcastReceiver.java
@@ -1,12 +1,12 @@
-package in.radongames.smsreceiver;
+package in.radongames.smsforwarder;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
+import android.text.TextUtils;
 import android.util.Log;
 
 import java.text.DateFormat;
@@ -19,8 +19,6 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
-import co.nedim.maildroidx.MaildroidX;
-import co.nedim.maildroidx.MaildroidXType;
 import lombok.CustomLog;
 
 @CustomLog
@@ -78,20 +76,14 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
                     }
                 }
 
-//                sendEmail(ctx, TextUtils.join(",", senderSet), bodyBuilder.toString(), getFormattedTime(minTime));
+                sendEmail(ctx, TextUtils.join(",", senderSet), bodyBuilder.toString(), getFormattedTime(minTime));
             }
         }
     }
 
     private SmsMessage createMessage(Object pdu, String format) {
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-            return SmsMessage.createFromPdu((byte[]) pdu, format);
-        } else {
-
-            return SmsMessage.createFromPdu((byte[]) pdu);
-        }
+        return SmsMessage.createFromPdu((byte[]) pdu, format);
     }
 
     private String getFormattedTime(long timestamp) {
@@ -114,40 +106,40 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
 
     private void sendEmail(Context ctx, String sender, String body, String time) {
 
-        Log.d("S2E", "From: " + sender);
+        log.debug("From: " + sender);
 
-        PrefManager pm = PrefManager.getInstance(ctx);
-        String email = pm.getUsername();
-
-        MaildroidX.Builder b = new MaildroidX.Builder()
-                .smtp(pm.getServer())
-                .smtpUsername(email)
-                .smtpPassword(pm.getPassword())
-                .port("465")
-                .type(MaildroidXType.PLAIN)
-                .to(email)
-                .from(email)
-                .subject("SMS From: " + sender)
-                .body("Received At: " + time + "\n===============\n" + body)
-                .onCompleteCallback(new MaildroidX.onCompleteCallback() {
-                    @Override
-                    public void onSuccess() {
-
-                        Log.d("S2E", "Email succeeded");
-                    }
-
-                    @Override
-                    public void onFail(String s) {
-
-                        Log.d("S2E", "Email failed");
-                    }
-
-                    @Override
-                    public long getTimeout() {
-
-                        return 60000; // 1 minute
-                    }
-                });
-        b.mail();
+//        PrefManager pm = PrefManager.getInstance(ctx);
+//        String email = pm.getUsername();
+//
+//        MaildroidX.Builder b = new MaildroidX.Builder()
+//                .smtp(pm.getServer())
+//                .smtpUsername(email)
+//                .smtpPassword(pm.getPassword())
+//                .port("465")
+//                .type(MaildroidXType.PLAIN)
+//                .to(email)
+//                .from(email)
+//                .subject("SMS From: " + sender)
+//                .body("Received At: " + time + "\n===============\n" + body)
+//                .onCompleteCallback(new MaildroidX.onCompleteCallback() {
+//                    @Override
+//                    public void onSuccess() {
+//
+//                        Log.d("S2E", "Email succeeded");
+//                    }
+//
+//                    @Override
+//                    public void onFail(String s) {
+//
+//                        Log.d("S2E", "Email failed");
+//                    }
+//
+//                    @Override
+//                    public long getTimeout() {
+//
+//                        return 60000; // 1 minute
+//                    }
+//                });
+//        b.mail();
     }
 }
