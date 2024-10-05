8d9b4f4bc929ea5eb4717a4e0bdb0e60c2871351
Binand Sethumadhavan <binand@radongames.in>
2024-10-05 12:15:30 +0530

    Finish switching to OlmurRecyclerView.

--- ReceiverApp/src/main/java/in/radongames/smsreceiver/ReceiverActivity.java
+++ ReceiverApp/src/main/java/in/radongames/smsreceiver/ReceiverActivity.java
@@ -1,18 +1,25 @@
 package in.radongames.smsreceiver;
 
+import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.radongames.android.olmur.OlmurDriver;
+import com.radongames.android.olmur.components.SwipeContextMenu;
+import com.radongames.android.olmur.property.ItemViewDecorator;
 import com.radongames.android.olmur.recyclerview.OlmurLayoutManager;
 import com.radongames.android.olmur.recyclerview.OlmurRecyclerView;
+import com.radongames.android.platform.Threader;
+import com.radongames.smslib.SmsContents;
 
 import javax.inject.Inject;
 
@@ -31,6 +38,9 @@ public class ReceiverActivity extends AppCompatActivity {
 
     @Inject
     SmsViewModel mSmsModel;
+
+    @Inject
+    Threader mThreader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,12 +75,55 @@ public class ReceiverActivity extends AppCompatActivity {
 
         rv.setLayoutManager(new OlmurLayoutManager(this));
         rv.setAdapter(mAdapter);
+        rv.addItemDecoration(new ItemViewDecorator() {
+            @Override
+            public void setViewProperties(View view, OlmurRecyclerView parent) {
+
+                int position = parent.getChildAdapterPosition(view);
+                view.setBackgroundResource(position % 2 == 0 ? R.color.message_even : R.color.message_odd);
+            }
+        });
+
+        OlmurDriver driver = new OlmurDriver.Builder()
+                .withSwipeRightAction(this::handleSwipe)
+                .withSwipeLeftAction(this::handleSwipe)
+                .withSwipeContextMenuDrawer(new SwipeContextMenu.Builder(this)
+                        .withBackgroundsColorsRes(R.color.colorPrimary, R.color.colorPrimary)
+                        .build())
+                .build();
+        driver.bind(rv);
 
         mSmsModel.getCount().observe(this, count -> log.debug("Loaded db with: " + count));
         mSmsModel.getMessages().observe(this, smsContents -> mAdapter.setMessages(smsContents));
     }
 
-    protected void copyFcmTokenToClipboard() {
+    private void handleSwipe(int position) {
+
+        log.debug("handleSwipe(): Deleting: " + position);
+        new AlertDialog.Builder(this)
+                .setTitle("Delete message at: " + position + "?")
+                .setCancelable(true)
+                .setPositiveButton(android.R.string.ok, (dialog, which) -> removeMessage(position))
+                .setNegativeButton(android.R.string.cancel, (dialog, which) -> handleCancel(position))
+                .setOnCancelListener(dialog -> handleCancel(position))
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    private void removeMessage(int position) {
+
+        final SmsContents message = mAdapter.removeMessage(position);
+        mThreader.runOnNewThread(() -> mSmsModel.deleteMessage(message));
+        log.debug("removeMessage(): Deleted: " + message);
+    }
+
+    private void handleCancel(int position) {
+
+        mAdapter.notifyItemChanged(position);
+        log.debug("handleCancel(): Not deleted: " + position);
+    }
+
+    private void copyFcmTokenToClipboard() {
 
         FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
 
